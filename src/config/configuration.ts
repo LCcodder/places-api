91d4a3edb2fbdef3e34646d9be62f401c2dd2a3d
Stepanyan Mikhail <toprobocopid@gmail.com>
2024-08-12 01:05:33 +0300

    Added config log at start

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,16 @@
-export default () => ({
-    mongodbUrl: "mongodb://localhost:27017/PlacesAPI",
-    masterKeys: ["sGJIPOSJGkgsodkgsoPKGoPSKGodpskgoDKGopskgodskgopdsk"],
-    jwtSecret: "dsafasfadfadsfdsafdsfdsfdssdfdsfdsf",
-    jwtExpiration: "24h",
+import { Logger } from '@nestjs/common';
+
+export default () => {
+  const logger = new Logger(`HTTP`);
+  return {
+    mongodbUrl: 'mongodb://localhost:27017/PlacesAPI',
+    masterKeys: ['sGJIPOSJGkgsodkgsoPKGoPSKGodpskgoDKGopskgodskgopdsk'],
+    jwtSecret: 'dsafasfadfadsfdsafdsfdsfdssdfdsfdsf',
+    jwtExpiration: '24h',
     log() {
-        
-    }
-});
+      logger.log(
+        `Loaded with config\nMONGO CONNECTION: ${this.mongodbUrl}\nJWT EXPIRATION: ${this.jwtExpiration}`,
+      );
+    },
+  };
+};
