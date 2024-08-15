21dec01c58622afb20d96d6b1f86d8b58bc4f777
Misha <toprobocopid@gmail.com>
2024-08-15 17:04:51 +0300

    Fixed mongodb connection issue

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,8 +4,8 @@ import { Controller, Get } from '@nestjs/common';
 export class AppController {
   constructor() {}
 
-  @Get()
+  @Get('ping')
   getHello(): string {
-    return 'ping';
+    return 'pong';
   }
 }
